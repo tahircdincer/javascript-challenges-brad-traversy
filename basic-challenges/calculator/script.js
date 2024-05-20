4cc1c78c8d7072384851c6785efda3103f8880c4
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-05-20 19:38:10 +0300

    Solution of basic challenge 3.

--- basic-challenges/calculator/script.js
+++ basic-challenges/calculator/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Also solution can be ' switch - case '
+
+const calculator = function (a, b, operator) {
+  if (operator === "*") {
+    return a * b;
+  } else if (operator === "/") {
+    return a / b;
+  } else if (operator === " - ") {
+    return a - b;
+  } else if (operator === "+") {
+    return a + b;
+  }
+};
