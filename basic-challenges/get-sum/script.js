8de695edbe4ec239228e6d2280f8d90abad5fbe8
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-05-20 19:16:51 +0300

    Solution of basic challenge 2.

---
+++ basic-challenges/get-sum/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const getSum = function (a, b) {
+  return a + b;
+};
+
+console.log(getSum(8, 4));
+
+// ( Beginning challenges are not that hard so sorry for my simple solution :) Later challenges I' ll do these things via loop or a method - reduce -. )
