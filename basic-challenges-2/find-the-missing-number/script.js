223f318bd8e1d90f8245792b3f7984ebf10e8dde
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-06-02 03:15:03 +0300

    Solution of ' find the missing number ' - basic challenges 2 section

---
+++ basic-challenges-2/find-the-missing-number/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// Array reduce uygulanır - Array' in en sağdaki ögesi * array' in en sağdaki ögesi + 1 / 2 reduce' a eşit değilse array toplamı - reduce
+
+const findMissingNumber = function (arr) {
+  const total = arr.reduce((curr, acc) => curr + acc, 0);
+  const n = arr.at(-1);
+  const totalSupposed = (n * (n + 1)) / 2;
+
+  if (arr.length === 0) {
+    return 1;
+  } else if (total !== totalSupposed) {
+    console.log(total, totalSupposed);
+    return totalSupposed - total;
+  }
+};
+
+console.log(findMissingNumber([0, 1, 2, 3, 4, 6, 7, 8]));
+console.log(findMissingNumber([1, 2, 3, 4, 6, 7, 8, 9, 10]));
+
+/*
+
+If substract the reduced amount from total supposed amount ( which is n * ( n + 1 )/ 2 ) it returns ' the ' number ' '.
+
+
+*/
